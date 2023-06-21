5b9f4339ea42021097a531521a2ed9f659ff49ec
Vinay K <vinayboi01@gmail.com>
2023-06-21 21:58:14 +0530

    asdf

--- server/server.js
+++ server/server.js
@@ -50,16 +50,17 @@ server.listen(port);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  console.log("HANDLE ERROR", err);
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
